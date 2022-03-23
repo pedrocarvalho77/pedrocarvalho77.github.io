51658396dab92c6541f9aad70dc2cb2c26ca6fe5
pedrocarvalho77 <pedromiguelrc99@gmail.com>
2022-03-23 19:29:04 +0000

    react-dummy ended, react-tutorial started

---
+++ react-tutorial/src/Table.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react'
+
+class Table extends Component {
+    render () {
+        const {characterData} = this.props
+        return (
+            <table>
+                <TableHeader />
+                <TableBody characterData={characterData}/>
+            </table>
+        )
+    }
+}
+
+const TableHeader = () => {
+    return (
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = () => {
+    return <tbody />
+}
+
+export default Table
